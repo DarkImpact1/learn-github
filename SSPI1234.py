3089b24d2a25deae7d6a771a776ece47c107bad7
DubeyJi <mohit.dev.new@gmail.com>
2025-05-05 00:47:31 +0530

    add new file SSPI1234

---
+++ SSPI1234.py
@@ -0,0 +1,6 @@
+def hello():
+    print("Hello, World!")
+    print("This is a simple Python script to demonstrate Git commands.")
+    print("You can use this script to learn and practice Git commands.")
+    print("Let's get started!")
+    
